f3ca04f31d5fd384b522a525b076a829b8568ece
wili1991 <wikki9106@gmail.com>
2023-02-01 20:24:26 -0500

    solucion5

---
+++ pedir_Datos_Por_Prompt_01022023/main.js
@@ -0,0 +1,2 @@
+let numero=prompt("ingresa un numero");
+alert(`El numero ingresado es ${numero}`);
--- todo_Sobre_Variables_01022023/main.js
+++
@@ -1,5 +0,0 @@
-let nombre="wilinton";
-let edad=31;
-let afiliado=true;
-
-alert(`Mi nombre es ${nombre} tengo ${edad} años y en comfenalco estoy afiliado ${afiliado}`);
