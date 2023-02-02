06826a182616691667230e512b14d7f60f17684f
wili1991 <wikki9106@gmail.com>
2023-02-01 20:09:17 -0500

    solucion1

---
+++ forma_Incluir_Javascript_01_01022023/main.js
@@ -0,0 +1,8 @@
+/*
+
+1.	Haz un programa de nombre main.js y otro llamado index.html incluir el archivo JavaScript en el <head></ head>, 
+que has de grabar en una carpeta llamada forma_Incluir_Javascript_01_FechaActual, mostrar un mensaje por medio de la 
+consola o de la ventana emergente un mensaje “Conexión con el archivo JavaScript Exitoso” .
+
+*/
+alert("Conexión con el archivo JavaScript Exitoso");
